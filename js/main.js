dee6c513829879c26e45d52dc150004de95861e3
Facundo Matias Caino <facundocaino@gmail.com>
2022-07-05 19:53:20 -0300

    funcion PasarDeAño

--- js/main.js
+++ js/main.js
@@ -1,48 +1,59 @@
-let valor_1 = 0;
-let valor_2 = 0;
-let valor_3 = 0;
-let valor_4 = 0;
-let valor_5 = 0;
-
-const alumno = {
-  nombre: valor_1,
-  apellido: valor_2,
-  nota_1: valor_3,
-  nota_2: valor_4,
-  nota_3: valor_5,
-};
-
 function PasarDeAño(alumno) {
-  for (let i = 0; i === 5; i += 1) {
-    let contador = 1;
-    switch (contador) {
-      case contador == 1:
-        let valor_1 = prompt("Ingrese el nombre del Alumno").toLocaleLowerCase;
-        contador += 1;
-      case contador == 2:
-        let valor_2 = prompt(
-          "Ingrese el apellido del Alumno"
-        ).toLocaleLowerCase;
-        contador += 1;
-      case contador == 3:
-        let valor_3 = prompt(
-          "Ingrese la primer nota del Alumno"
-        ).toLocaleLowerCase;
-        contador += 1;
-      case contador == 4:
-        let valor_4 = prompt(
-          "Ingrese la segunda nota Alumno"
-        ).toLocaleLowerCase;
-        contador += 1;
-      case contador == 5:
-        let valor_5 = prompt(
-          "Ingrese la tercer nota del Alumno"
-        ).toLocaleLowerCase;
-        break;
-      default:
-        console.log("Error, Intente de Nuevo");
+  let nota_final = alumno.nota_1 + alumno.nota_2 + alumno.nota_3 / 3;
+  if (nota_final >= 6) {
+    return (
+      "El alumno " + alumno.nombre + " " + alumno.apellido + " aprobo el año"
+    );
+  }
+  if (nota_final < 6) {
+    let final = parseInt(
+      prompt(
+        "El alumno " +
+          alumno.nombre +
+          " " +
+          alumno.apellido +
+          " no logro aprobo el año y debe rendir un final. " +
+          "Ingrese la nota del final *Numero del 0-10*"
+      )
+    );
+    if (final >= 4) {
+      return (
+        "El alumno " +
+        alumno.nombre +
+        " " +
+        alumno.apellido +
+        " aprobo el final, logro pasar de año"
+      );
+    } else if (final < 4) {
+      return (
+        "El alumno " +
+        alumno.nombre +
+        " " +
+        alumno.apellido +
+        " desaprobo el final, no logro pasar de año y debe repetir el año"
+      );
     }
   }
 }
 
-PasarDeAño();
+const alumno = {
+  nombre: String(prompt("Ingrese el nombre del alumno")),
+  apellido: String(prompt("Ingrese el apellido del alumno")),
+  nota_1: parseInt(
+    prompt("Ingrese la primer nota del alumno *Numero del 0-10*")
+  ),
+  nota_2: parseInt(
+    prompt("Ingrese la segunda nota del alumno *Numero del 0-10*")
+  ),
+  nota_3: parseInt(
+    prompt("Ingrese la tercera nota del alumno *Numero del 0-10*")
+  ),
+  /* nombre: "facundo",
+  apellido: "caino",
+  nota_1: 1,
+  nota_2: 1,
+  nota_3: 1,*/
+};
+
+const respuesta = PasarDeAño(alumno);
+console.log(respuesta);
