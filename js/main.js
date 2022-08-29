4924f3fc5fb5681ea79d9f390f5e240a9fa20935
Facundo Matias Caino <facundocaino@gmail.com>
2022-08-29 18:08:33 -0300

    1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-/**  boton del menu desplegable movil **/
-let menu = false;
+// Crear cuenta  //
+let crear_tu_cuenta = document.getElementById(crear - tu - cuenta);
+let crear_cuenta_part1 = document.getElementById(crear - cuenta - part1);
+let crear_cuenta_part2 = document.getElementById(crear - cuenta - part2);
+let iniciar_sesion_part1 = document.getElementById(iniciar - sesion - part1);
+let iniciar_sesion_part2 = document.getElementById(iniciar - sesion - part2);
 
-function mostrarMenu() {
-  if (menu === false) {
-    document.getElementById("menu-desplegable").style.display = "block";
-    menu = true;
-  } else {
-    document.getElementById("menu-desplegable").style.display = "none";
-    menu = false;
-  }
-}
+function crear_cuenta() {}
