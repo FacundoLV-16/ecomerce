0bdbf4d5fbfa582697547785f1a0f57e004d8d40
Facundo Matias Caino <facundocaino@gmail.com>
2022-08-31 14:48:10 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 // Crear cuenta  //
-function crear_cuenta() {
-  let x = document.getElementById(crear_cuenta_part1);
-  if (x.style.display === "block") {
-    x.style.display === "none";
-  } else {
-    x.style.display === "none";
-  }
-}
+
+/*let btn = (document.getElementById("btn-crear-cuenta").onclick =
+  function crear_cuenta() {
+    let formulario_crear = document.getElementById("crear-cuenta-part1");
+    let formulario_creado = document.getElementById("crear-cuenta-part2");
+    if (formulario_crear.style.display === "block") {
+      formulario_crear.style.display === "none";
+      formulario_creado.style.display === "block";
+    }
+  });*/
+
+let btn = (document.getElementById("btn-crear-cuenta").onclick =
+  function crear_cuenta() {
+    if (
+      document.getElementById("crear-cuenta-part1").style.display === "block"
+    ) {
+      document.getElementById("crear-cuenta-part1").style.display === "none";
+      document.getElementById("crear-cuenta-part2").style.display === "block";
+    }
+  });
