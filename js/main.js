d42275179cc73140490a064d2eec62afcb66de09
Facundo Matias Caino <facundocaino@gmail.com>
2022-08-31 14:25:58 -0300

    register-login

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // Crear cuenta  //
-let crear_tu_cuenta = document.getElementById(crear - tu - cuenta);
-let crear_cuenta_part1 = document.getElementById(crear - cuenta - part1);
-let crear_cuenta_part2 = document.getElementById(crear - cuenta - part2);
-let iniciar_sesion_part1 = document.getElementById(iniciar - sesion - part1);
-let iniciar_sesion_part2 = document.getElementById(iniciar - sesion - part2);
-
-function crear_cuenta() {}
+function crear_cuenta() {
+  let x = document.getElementById(crear_cuenta_part1);
+  if (x.style.display === "block") {
+    x.style.display === "none";
+  } else {
+    x.style.display === "none";
+  }
+}
